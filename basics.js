649eebce98f5de7ec79701aa365c2c2e4ced9245
Yaseeeen <halfduplex001@gmail.com>
2024-02-19 16:57:58 +0530

    first commit

---
+++ basics.js
@@ -0,0 +1,37 @@
+
+// let fruits=["apple","banana","orange"]
+// console.log(fruits[1])
+// console.log(fruits.length)
+// fruits[2]="grape"
+// console.log(fruits)
+// fruits.push("mango")
+// console.log(fruits)
+// fruits.unshift("ft")
+// console.log(fruits)
+// fruits.shift("banana")
+// console.log(fruits)
+// fruits.shift("mango")
+// console.log(fruits)
+// function greet(name)
+// {
+//     console.log("welcome"+" "+name+"!")
+// }
+// greet("nandana")
+// function add(a,b)
+// {
+//     return a+b;
+
+// }
+// console.log(add(5,6))
+// 
+// var add=(a,b)=>{
+//     var sum=a+b;
+//     console.log(sum)
+//     console.log(`sum is  ${sum}`)
+// };
+// add(3,4);
+var a=[1,2,3]
+var b=[5,4,6]
+var c=[...a,...b]
+var [a,b,...t]=[1,2,3,4,5,6,7,8,9]
+console.log(t)
